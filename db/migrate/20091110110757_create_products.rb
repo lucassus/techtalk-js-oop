43eca8f5a08e75682eba5d5d93a387442a76208d
Łukasz Bandzarewicz <lucassus@gmail.com>
2009-11-10 12:12:27 +0100

    Create products and categories.

---
+++ db/migrate/20091110110757_create_products.rb
@@ -0,0 +1,19 @@
+class CreateProducts < ActiveRecord::Migration
+  def self.up
+    create_table :products do |t|
+      t.integer :category_id
+      t.string :name, :null => false
+      t.text :description
+      t.decimal :price, :null => false
+      t.boolean :promotion
+
+      t.timestamps
+    end
+
+    add_index(:products, :category_id)
+  end
+
+  def self.down
+    drop_table :products
+  end
+end
